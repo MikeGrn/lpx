7c802631382755afe2d799cb4b06f4c22e762c67
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2018-06-29 15:07:44 +0700

    Порефакторил обработку ХТТП запросов

--- lpx-server/src/main.c
+++ lpx-server/src/main.c
@@ -15,6 +15,9 @@
 
 #define PORT 8888
 
+#define BAD_REQUEST 1
+#define INTERNAL_ERROR 2
+
 typedef struct LpxServer {
     Storage *storage;
 } LpxServer;
@@ -86,7 +89,101 @@ static int not_found(struct MHD_Connection *connection) {
     return ret;
 }
 
-static int handle_stream(LpxServer *lpx, struct MHD_Connection *connection, const char *url, const char *method) {
+static int8_t
+open_stream_frames(LpxServer *lpx, char *stream_id, List *frame_times_str, VideoStreamBytesStream **stream,
+                   char **err_msg) {
+    int8_t res = 0;
+    size_t frames_cnt = lst_size(frame_times_str);
+    ListIter *iter = lst_iterator(frame_times_str);
+    uint64_t *frame_times = xcalloc(frames_cnt, sizeof(uint64_t));
+    for (int i = 0; lst_iter_advance(iter); i++) {
+        char *null;
+        int64_t sframe_time = strtoll(lst_iter_peak(iter), &null, 10);
+        if (sframe_time == LLONG_MIN || sframe_time == LLONG_MAX || *null != 0) {
+            *err_msg = "invalid frame_time GET parameter expected";
+            res = BAD_REQUEST;
+            goto free_iter;
+        }
+        frame_times[i] = (uint64_t) sframe_time;
+    }
+    qsort(frame_times, frames_cnt, sizeof(uint64_t), uint64_t_cmp);
+    FrameMeta **index = NULL;
+    size_t index_size = 0;
+    int8_t r = storage_read_stream_idx(lpx->storage, stream_id, &index, &index_size);
+    if (r != LPX_SUCCESS) {
+        res = INTERNAL_ERROR;
+        goto free_iter;
+    }
+
+    List *frame_indexes = lst_create();
+    for (int i = 0; i < frames_cnt; i++) {
+        ssize_t sidx = stream_find_frame(index, index_size, frame_times[i]);
+        if (sidx < 0) {
+            // запросили оффсет больше, чем конец последнего фрейма
+            break;
+        }
+        size_t *idx = xmalloc(sizeof(size_t));
+        *idx = (size_t) sidx;
+        lst_append(frame_indexes, (const void *) idx);
+    }
+    res = storage_open_stream_frames(lpx->storage, stream_id, frame_indexes, stream);
+    if (res != LPX_SUCCESS) {
+        res = INTERNAL_ERROR;
+    }
+
+    lst_deep_free(frame_indexes);
+    free_array((void **) index, index_size);
+
+    free_iter:
+    lst_iter_free(iter);
+    free(frame_times);
+
+    return res;
+}
+
+static int8_t
+open_stream_offset(LpxServer *lpx, struct MHD_Connection *connection, char *stream_id, VideoStreamBytesStream **stream,
+                   char **err_msg) {
+    size_t offset = 0;
+    const char *offset_str = MHD_lookup_connection_value(connection, MHD_GET_ARGUMENT_KIND, "offset");
+    if (offset_str == NULL) {
+        offset = 0;
+    } else {
+        char *null;
+        ssize_t soffset = strtoll(offset_str, &null, 10);
+        if (soffset == LLONG_MIN || soffset == LLONG_MAX || *null != 0 || soffset < 0) {
+            *err_msg = "invalid offset GET parameter";
+            return BAD_REQUEST;
+        }
+        offset = (size_t) soffset;
+    }
+
+    int8_t res = storage_open_stream(lpx->storage, stream_id, offset, stream);
+    if (res != LPX_SUCCESS) {
+        return INTERNAL_ERROR;
+    }
+}
+
+static int8_t
+open_stream(LpxServer *lpx, struct MHD_Connection *connection, char *stream_id, VideoStreamBytesStream **stream,
+            char **err_msg) {
+    int8_t res = 0;
+    ValuesIter iter = {.res = lst_create(), .key = "frame_time"};
+    MHD_get_connection_values(connection, MHD_GET_ARGUMENT_KIND, list_parameters, &iter);
+    if (lst_size(iter.res) > 0) {
+        res = open_stream_frames(lpx, stream_id, iter.res, stream, err_msg);
+    } else {
+        res = open_stream_offset(lpx, connection, stream_id, stream, err_msg);
+    }
+
+    lst_free(iter.res);
+
+    return res;
+}
+
+static int handle_stream(LpxServer *lpx, struct MHD_Connection *connection) {
+    int ret = 0;
+
     const char *stream_time_str = MHD_lookup_connection_value(connection, MHD_GET_ARGUMENT_KIND, "stream_time");
     if (stream_time_str == NULL) {
         return bad_request(connection, "stream_time GET parameter expected");
@@ -99,83 +196,48 @@ static int handle_stream(LpxServer *lpx, struct MHD_Connection *connection, cons
     }
 
     char *stream_id = NULL;
-    storage_find_stream(lpx->storage, time, &stream_id);
+    int8_t res = storage_find_stream(lpx->storage, time, &stream_id);
+    if (res != LPX_SUCCESS) {
+        ret = internal_error(connection);
+        goto free_stream_id;
+    } else if (stream_id == NULL) {
+        return not_found(connection);
+    }
 
-    // todo: free
-    ValuesIter iter = {.res = lst_create(), .key = "frame_time"};
-    MHD_get_connection_values(connection, MHD_GET_ARGUMENT_KIND, list_parameters, &iter);
     VideoStreamBytesStream *stream = NULL;
-    size_t frames_cnt = lst_size(iter.res);
-    if (frames_cnt > 0) {
-        const char **frame_times_str = xcalloc(frames_cnt, sizeof(char *));
-        lst_to_array(iter.res, (const void **) frame_times_str);
-
-        uint64_t *frame_times = xcalloc(frames_cnt, sizeof(uint64_t));
-        for (int i = 0; i < frames_cnt; i++) {
-            int64_t sframe_time = strtoll(frame_times_str[i], &null, 10);
-            if (sframe_time == LLONG_MIN || sframe_time == LLONG_MAX || *null != 0) {
-                return bad_request(connection, "invalid frame_time GET parameter expected");
-            }
-            frame_times[i] = (uint64_t) sframe_time;
-        }
-        free(frame_times_str);
-        qsort(frame_times, frames_cnt, sizeof(uint64_t), uint64_t_cmp);
-        FrameMeta **index = NULL;
-        size_t index_size = 0;
-        storage_read_stream_idx(lpx->storage, stream_id, &index, &index_size);
-        List *frame_indexes = lst_create();
-        for (int i = 0; i < frames_cnt; i++) {
-            ssize_t sidx = stream_find_frame(index, index_size, frame_times[i]);
-            if (sidx < 0) {
-                // запросили оффсет больше, чем конец последнего фрейма
-                break;
-            }
-            size_t *idx = xmalloc(sizeof(size_t));
-            *idx = (size_t) sidx;
-            lst_append(frame_indexes, (const void *) idx);
-        }
-        free(frame_times);
-        int8_t res = storage_open_stream_frames(lpx->storage, stream_id, frame_indexes, &stream);
-        lst_deep_free(frame_indexes);
-        free_array((void **) index, index_size);
-        if (res != LPX_SUCCESS) {
-            return internal_error(connection);
-        }
-        lst_free(iter.res);
-    } else {
-        size_t offset = 0;
-        const char *offset_str = MHD_lookup_connection_value(connection, MHD_GET_ARGUMENT_KIND, "offset");
-        if (offset_str == NULL) {
-            offset = 0;
-        } else {
-            ssize_t soffset = strtoll(offset_str, &null, 10);
-            if (soffset == LLONG_MIN || soffset == LLONG_MAX || *null != 0 || soffset < 0) {
-                return bad_request(connection, "invalid offset GET parameter");
-            }
-            offset = (size_t) soffset;
-        }
-
-        if (stream_id == NULL) {
-            return not_found(connection);
-        }
-        int8_t res = storage_open_stream(lpx->storage, stream_id, offset, &stream);
-        if (res != LPX_SUCCESS) {
-            return internal_error(connection);
-        }
+    char *err_msg = NULL;
+    res = open_stream(lpx, connection, stream_id, &stream, &err_msg);
+    if (res == BAD_REQUEST) {
+        ret = bad_request(connection, err_msg);
+        goto free_stream_id;
+    } else if (res == INTERNAL_ERROR) {
+        ret = internal_error(connection);
+        goto free_stream_id;
+    } else if (res != LPX_SUCCESS) {
+        assert(false);
     }
 
     struct MHD_Response *response;
 
     response = MHD_create_response_from_callback(MHD_SIZE_UNKNOWN, 1024, stream_reader_callback, stream,
                                                  stream_close_callback);
-    MHD_add_response_header(response, "Content-Type", "application/zip");
+    ret = MHD_add_response_header(response, "Content-Type", "application/zip");
+    if (ret != MHD_YES) {
+        goto free_stream_id;
+    }
     char *filename = xcalloc(1024, sizeof(char));
     sprintf(filename, "attachment; filename=\"%s.bin\"", stream_id);
-    MHD_add_response_header(response, "Content-Disposition", filename);
-    free(filename);
-    int ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+    ret = MHD_add_response_header(response, "Content-Disposition", filename);
+    if (ret != MHD_YES) {
+        goto free_filename;
+    }
+    ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
     MHD_destroy_response(response);
 
+    free_filename:
+    free(filename);
+
+    free_stream_id:
     free(stream_id);
 
     return ret;
@@ -196,7 +258,7 @@ static int answer_to_connection(void *cls, struct MHD_Connection *connection,
     }
 
     if (strcmp(url, "/stream") == 0) {
-        return handle_stream(lpx, connection, url, method);
+        return handle_stream(lpx, connection);
     } else {
         const char *page = "No such function";
         struct MHD_Response *response;
